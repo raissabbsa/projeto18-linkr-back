d829b942e7e9193211ad2f77dd9ad8b5cdddf14a
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2023-01-06 03:14:42 -0300

    feat: using lib to get link metadata

--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -1,17 +1,17 @@
 import { connectionDB } from "../database/db.js";
 
-export function createPost(user, infos) {
+export function createPost(user, infos, data) {
   return connectionDB.query(
     `
-    INSERT INTO posts (user_id, link, description, likes) 
-        VALUES ($1, $2, $3, $4)`,
-    [user.id, infos.link, infos.description, 0]
+    INSERT INTO posts (user_id, description, likes, link, link_title, link_description, link_image)
+        VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+    [user.id, infos.description, 0, infos.link, data.title, data.description, data.icon]
   );
 }
 
 export function getPost() {
   return connectionDB.query(`
-    SELECT posts.*, 
+    SELECT posts.*,
     users.picture_url AS picture_user,
     users.username
     FROM posts
@@ -22,7 +22,7 @@ export function getPost() {
 
 export function updatePost(body) {
   return connectionDB.query(
-    `UPDATE posts 
+    `UPDATE posts
         SET description = $1 WHERE id = $2`,
     [body.description, body.id]
   );
