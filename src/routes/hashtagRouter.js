7bfb6570fd5614d3d06e978a63abe4356784beae
Dayane <dayanelaura3@gmail.com>
2023-01-08 06:41:50 -0300

    mend

--- src/routes/hashtagRouter.js
+++ src/routes/hashtagRouter.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { authValidation } from "../middlewares/authValidationMiddleware.js";
-//import { hashtagValidation } from "../middlewares/hashtagValidationMiddleware.js";
 import { sendPostsByHashtag } from "../controllers/hashtagControllers.js";
 
-const trendingRouter = Router();
+const hashtagRouter = Router();
 
-trendingRouter.get('/hashtag/:hashtag', authValidation, sendPostsByHashtag);
+hashtagRouter.get('/hashtag/:hashtag', authValidation, sendPostsByHashtag);
 
-export default trendingRouter;
+export default hashtagRouter;
