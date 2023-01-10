3243370057468e98d1ab4f214581ed9a3fdc8b42
Francisco Antonio Paiva <fcooantoniops@gmail.com>
2023-01-10 11:40:32 -0300

    fix and refactor: routes likes  and isliked

--- src/routes/likeRouter.js
+++ src/routes/likeRouter.js
@@ -7,6 +7,5 @@ const likeRouter = Router();
 likeRouter.post("/like", authValidation, like);
 likeRouter.delete("/dislike/:id", authValidation, dislike);
 likeRouter.get("/likes", authValidation, getLikes);
-likeRouter.get("/isliked", authValidation, getIsLiked)
 
 export default likeRouter;
